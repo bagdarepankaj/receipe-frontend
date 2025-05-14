141a7dd5ec8a749effe4ad5c4e688a64fa7e0fb2
Pankaj Bagdare <pankajbagdare@gmail.com>
2025-05-14 19:09:11 +0530

    Added login component

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,14 +1,34 @@
 import * as React from 'react';
-import { Paper, TextField } from '@mui/material';
+import { Button, Paper, TextField } from '@mui/material';
 
 export default function Login() {
+
+    let [email, setEmail] = React.useState('');
+    let [password, setPassword] = React.useState('');
+    let [login, setLogin] = React.useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response =  await fetch('http://localhost:4000/login', {   
+            method: 'post',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({ email, password })
+        })
+        const result =  await response.json()
+        setLogin(result.success)
+
+        console.log('result', result)
+    }
+
     return (
         <Paper elevation={6} className='login-box'>
             <span style={{fontSize: 'x-large'}}>Receipe Book</span>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='login-form'>
-                    <TextField id="email" label="Email" variant="standard" sx={{width: '100%', margin: '5px 0'}} />
-                    <TextField id="password" label="Password" variant="standard" sx={{width: '100%', margin: '5px 0'}} />
+                    <TextField id="email" name="email" label="Email" variant="standard" onChange={(e)=> setEmail(e.target.value)} sx={{width: '100%', margin: '8px 0'}} />
+                    <TextField id="password" name="password" label="Password" type= 'password' onChange={(e)=> setPassword(e.target.value)} variant="standard" sx={{width: '100%', margin: '8px 0'}} />
+                    <Button type='submit' variant='contained' color='success' sx={{marginTop: '10px'}}>Login</Button>
                 </div>
             </form>
         </Paper>
